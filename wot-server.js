bed688721ce8c04014043a0e581c8965447c63ae
Mikhail Falaleev <falaleevms@gmail.com>
2018-03-09 12:23:04 +0000

    add real support for pir and temperature&humidity sensor

--- wot-server.js
+++ wot-server.js
@@ -1,6 +1,14 @@
 var httpServer = require('./servers/http'),
- resources = require('./resources/model');
- var server = httpServer.listen(resources.pi.port, function () {
-console.info('Your WoT Pi is up and running on port %s',
-resources.pi.port);
+    resources = require('./resources/model');
+
+var pirPlugin = require('./plugins/internal/pirPlugin'),
+    dhtPlugin = require('./plugins/internal/DHT11Plugin.js');
+    // ledsPlugin = require('./plugins/ledsPlugin');
+
+ pirPlugin.start({'simulate': false, 'frequency': 2000});
+ dhtPlugin.start({'simulate': false, 'frequency': 10000});
+
+var server = httpServer.listen(resources.pi.port, function () {
+    console.info('Your WoT Pi is up and running on port %s',
+    resources.pi.port);
 });
